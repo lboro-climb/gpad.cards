1b2fe38a6ab6a51debf6a087cc2cbe9d0ad9a027
Jonah Thomas <j.j.c.thomas@lboro.ac.uk>
2022-02-03 11:39:36 +0000

    add basic documentation for datasets data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' The datasets identified as part of the review that formed the GPAD resource
+#'
+#' The dataset contains the datasets that were identified as part of the GPAD resource
+#'
+#' @format A data frame with 105 rows and 114 variables
+#' 
+#' 
+"datasets"
